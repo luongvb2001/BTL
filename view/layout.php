c632b73fd728828e2dde613498e28a6d1f46cd9c
PHUONG.DD194141@sis.hust.edu.vn <PHUONG.DD194141@sis.hust.edu.vn>
2022-06-30 20:47:57 +0700

    first commit

---
+++ view/layout.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <Title>Nha hang Wibu</Title>
+        <link rel="stylesheet" href="../css/layout.css">
+    <header>KAME</header>
+    <body>
+        <nav class="navbar">
+            <a href="home.php">Home</a> 
+            <a href="vipmember.php">Vipmember</a> 
+            <a href="statistical.php">Statistical</a>
+            <a href="setting.php">Setting</a>
+            <a href="logout.php">Logout</a>
+        </nav>
+    </body>
+    <footer>
+    </footer>
+</html>
