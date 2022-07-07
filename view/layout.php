f9786e606a78961b75fa433a1563904e4c5075b9
LuongVB <luonghc2001@gmail.com>
2022-07-07 14:21:39 +0700

    navbar

--- view/layout.php
+++ view/layout.php
@@ -2,6 +2,15 @@
     <head>
         <Title>Wibu Restaurant</Title>
         <link rel="stylesheet" href="../css/layout.css">
+        <style>
+            body{
+                width: 100%;
+                height: 100vh;
+                box-sizing: border-box;
+                background: url(https://scontent.fhan14-1.fna.fbcdn.net/v/t1.15752-9/291228371_2173178152842057_9086105916767756183_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=ae9488&_nc_ohc=vqYeUNGhnfMAX-ZeCIQ&_nc_ht=scontent.fhan14-1.fna&oh=03_AVJRNVj4MwGxUZmqZVN_gVudgFjzZ9qR6tw8WJkcoSl2fA&oe=62EAF7BB);
+                background-size: cover;
+            }
+        </style>
     </head>
     <body>
         <nav>
@@ -16,7 +25,6 @@
                 <li><a href="logout.php" aria-hidden="true">Logout</a></li>
             </ul>
         </nav>
-        <section class="section1"></section>
     </body>
     <footer>
     </footer>
